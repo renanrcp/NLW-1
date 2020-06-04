0f2e67c45f3c7469030c072834a5e22da6e96397
renanrcp <renancamerapinheiro@gmail.com>
2020-06-04 02:02:33 -0300

    post points without relations

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -8,12 +8,39 @@ routes.get('/items', async (req, res) => {
 
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image: `http://localhost:3333/uploads/${item.image}`,
         };
     });
 
     return res.json(serializedItems);
+});
+
+routes.post('/points', async (req, res) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = req.body;
+
+    await knex('points').insert({
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        image: 'image-fake',
+    });
+
+    return res.json({ sucess: true });
 });
 
 export default routes;
