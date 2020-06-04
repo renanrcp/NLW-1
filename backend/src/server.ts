f74d5f844e19bf1eeda236815b0090203c15ad42
renanrcp <renancamerapinheiro@gmail.com>
2020-06-03 23:36:35 -0300

    node-dev

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,14 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', () => {
+app.get('/users', (req, res) => {
     console.log('Listagem de usuários.');
+
+    res.json([
+        'Diego',
+        'Cleiton',
+        'Robson'
+    ]);
 });
 
 app.listen(3333);
