a230151078e88dc573362fcf2d1ae8610247a19a
renanrcp <renancamerapinheiro@gmail.com>
2020-06-04 02:43:01 -0300

    new routes points

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -40,10 +40,51 @@ class PointsController {
 
         await trx('point_items').insert(point_items);
 
+        await trx.commit();
+
         return res.json({
             id: point_id,
             ...point,
         });
+    };
+
+    async show(req: Request, res: Response) {
+        const { id } = req.params;
+
+        const point = await knex('points').where('id', id).first();
+
+        if (!point) {
+            return res.status(400).json({ message: 'Point not found' });
+        };
+
+        const items = await knex('items')
+            .join('point_items', 'items.id', '=', 'point_items.item_id')
+            .where('point_items.point_id', id);
+
+        return res.json({ point, items });
+    };
+
+    async index(req: Request, res: Response) {
+        const {
+            city,
+            uf,
+            items
+        } = req.query;
+
+        const parsedItems = String(items)
+            .split(',')
+            .map(item => Number(item.trim()));
+
+        const points = await knex('points')
+            .join('point_items', 'points.id', '=', 'point_items.point_id')
+            .whereIn('point_items.item_id', parsedItems)
+            .where('city', String(city))
+            .where('uf', String(uf))
+            .distinct()
+            .select('points.*');
+
+
+        return res.json(points);
     };
 };
 
