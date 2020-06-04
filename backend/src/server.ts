de3e629d014ac9530d3c98c9a3f0f04d6f5e627e
renanrcp <renancamerapinheiro@gmail.com>
2020-06-04 00:24:27 -0300

    routes and connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,37 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-const users = [
-    'Diego',
-    'Cleiton',
-    'Robson'
-]
-
-app.get('/users', (req, res) => {
-    const search = String(req.query.search);
-
-    const filteredUsers = search
-        ? users.filter(a => a.includes(search))
-        : users;
-
-    return res.json(filteredUsers);
-});
-
-app.get('/users/:id', (req, res) => {
-    const id = Number(req.params.id);
-
-    const user = users[id];
-
-    return res.json(id);
-});
-
-app.post('/users', (req, res) => {
-    const data = req.body;
-
-    return res.json(data);
-});
+app.use(routes);
 
 app.listen(3333);
