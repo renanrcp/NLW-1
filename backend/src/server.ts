8cba19defd35abb0c8177ccf3dddc6250865c715
renanrcp <renancamerapinheiro@gmail.com>
2020-06-04 00:16:27 -0300

    new routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,14 +2,36 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    console.log('Listagem de usuários.');
+app.use(express.json());
 
-    res.json([
-        'Diego',
-        'Cleiton',
-        'Robson'
-    ]);
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robson'
+]
+
+app.get('/users', (req, res) => {
+    const search = String(req.query.search);
+
+    const filteredUsers = search
+        ? users.filter(a => a.includes(search))
+        : users;
+
+    return res.json(filteredUsers);
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    const user = users[id];
+
+    return res.json(id);
+});
+
+app.post('/users', (req, res) => {
+    const data = req.body;
+
+    return res.json(data);
 });
 
 app.listen(3333);
