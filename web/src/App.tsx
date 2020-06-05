0d900aa3823060e47be338fa9557311d3a885b00
renanrcp <renancamerapinheiro@gmail.com>
2020-06-05 00:52:02 -0300

    counter

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./App.css";
 
+import Header from "./Header";
+
 function App() {
-  return <h1>Hello World</h1>;
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <Header title={`Contador: ${counter}`} />
+
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>
+        Aumentar
+      </button>
+    </div>
+  );
 }
 
 export default App;
