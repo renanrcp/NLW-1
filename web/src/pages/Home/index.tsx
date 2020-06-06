59d473183278586f3f10b0867d469cfe4e3dbc0f
renanrcp <renancamerapinheiro@gmail.com>
2020-06-05 23:38:14 -0300

    icons and initial home elements

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+import "./styles.css";
+
+import logo from "../../assets/logo.svg";
+
 const Home = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logo} alt="Ecoleta" />
+        </header>
+
+        <main>
+          <h1>Seu marketplace de coleta de resíduos.</h1>
+          <p>
+            Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+          </p>
+
+          <a href="/cadastron">
+            <span>></span>
+            <strong>Cadastre um ponto de coleta.</strong>
+          </a>
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
