b4ebfd6f2858bb6dc1fd65dd09fe0d9da22eb755
renanrcp <renancamerapinheiro@gmail.com>
2020-06-05 23:31:01 -0300

    home init

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./App.css";
 
-function App() {
-  return <h1>Hello World</h1>;
-}
+import Home from "./pages/Home";
+
+const App = () => {
+  return <Home />;
+};
 
 export default App;
